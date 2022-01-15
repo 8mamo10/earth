359036fe0e446ea3437adce37691a3f48dac08c3
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-01-15 13:21:48 +0900

    Refactor

--- src/landsat8_showing.js
+++ src/landsat8_showing.js
@@ -1,9 +1,15 @@
 var start = ee.Date('2017-01-01');
 var end = ee.Date('2017-01-31');
-var landsat8_img = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR').filterDate(start, end);
+var lon = 140;
+var lat = 36;
+var point = ee.Geometry.Point(lon, lat);
+
+var landsat8_img = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+  .filterBounds(point)
+  .filterDate(start, end);
 
 // Center the Map.
-Map.setCenter(140, 36, 8);
+Map.setCenter(lon, lat, 8);
 // Display the image.
 var vizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
 Map.addLayer(landsat8_img, vizParams);
