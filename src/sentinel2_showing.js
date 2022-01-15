9106415c12cdcf5fc6518828314ae50c9dde8e27
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-01-15 15:58:36 +0900

    Show landsat and sentinel on the same view

--- src/sentinel2_showing.js
+++ src/sentinel2_showing.js
@@ -7,5 +7,13 @@ var point = ee.Geometry.Point(lon, lat);
 var sentinel2_img = sentinel2ImageCollection
   .filterBounds(point)
   .filterDate(start, finish);
-var vizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
-Map.addLayer(sentinel2_img, vizParams);
+
+var landsat8_img = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+  .filterBounds(point)
+  .filterDate(start, finish);
+
+Map.setCenter(lon, lat, 13);
+var sentinel2VizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
+var landsat8VizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
+Map.addLayer(sentinel2_img, sentinel2VizParams, "sentinel2");
+Map.addLayer(landsat8_img, landsat8VizParams, "landsat8");
