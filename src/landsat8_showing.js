706adad7f60ebf923e632359ffb6bfe455585b06
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2022-01-16 23:26:44 +0900

    Use several colors

--- src/landsat8_showing.js
+++ src/landsat8_showing.js
@@ -11,5 +11,9 @@ var landsat8_img = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
 // Center the Map.
 Map.setCenter(lon, lat, 8);
 // Display the image.
-var vizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
-Map.addLayer(landsat8_img, vizParams);
+var trueColorVizParams = { bands: ['B4', 'B3', 'B2'], min: 0, max: 3000 };
+var naturalColorVizParams = { bands: ['B4', 'B5', 'B2'], min: 0, max: 3000 };
+var falseColorVizParams = { bands: ['B5', 'B4', 'B3'], min: 0, max: 3000 };
+Map.addLayer(landsat8_img, trueColorVizParams, "True color");
+Map.addLayer(landsat8_img, naturalColorVizParams, "Natural color");
+Map.addLayer(landsat8_img, falseColorVizParams, "False color");
